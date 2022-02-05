3a2a7db34a0ea93efe4b5c10d8ae6deaecd90a95
Katie Sexton <ksexton@adobe.com>
2022-02-04 22:56:13 -0700

    style links in big text block

--- assets/blocks/big-text/big-text.js
+++ assets/blocks/big-text/big-text.js
@@ -1,3 +1,8 @@
 export default function decorateBigText(block) {
-  block.classList.add('big-text');
+  block.querySelectorAll('a').forEach((a) => {
+    const parent = a.parentNode;
+    if (parent.textContent === a.textContent) {
+      a.classList.add('btn', 'btn-rect');
+    }
+  });
 }
