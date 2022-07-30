e05981d02e81f7efec39f9b8f9c12c2d21cce925
Katie Sexton <ksexton@adobe.com>
2022-07-29 21:47:14 -0600

    feat: wholesale ordering conf and order periods

--- assets/pages/order/order.js
+++ assets/pages/order/order.js
@@ -6,6 +6,7 @@ import {
   buildGQs,
   loadCSS,
   noScroll,
+  fetchLabels,
 } from '../../scripts/scripts.js';
 
 import {
@@ -26,11 +27,11 @@ import {
 
 import {
   buildScreensaver,
-  // makeScreensaverError,
-  // removeScreensaver,
+  makeScreensaverError,
+  removeScreensaver,
 } from '../../utils/screensaver/screensaver.js';
 
-const WHOLESALE_KEY = 'AKfycbzrxcKi-uFAyufJw5uWdcwkj0w1aA6PKasQ2agsdAMw4m8yq9D5GLQ-3BjaU8ocexEiwg';
+const WHOLESALE_KEY = 'AKfycbwp-V9KppEYT1GjXmr59_Lk3wSmKSnlDeOWuggEC_DmyVptfRl2MpiQn9FDowzzQdQE';
 
 function buildBackBtn(menu) {
   menu.classList.add('relative');
@@ -74,61 +75,40 @@ function buildMenu(block) {
 }
 
 async function fetchWholesaleData() {
-  if (!window.wholesaleData) {
-    const url = buildGScriptLink(WHOLESALE_KEY);
-    const resp = await fetch(url);
-    if (resp.ok) {
-      const json = await resp.json();
-      window.wholesaleData = json;
-    }
+  const url = buildGScriptLink(WHOLESALE_KEY);
+  const resp = await fetch(url, { cache: 'reload' });
+  if (resp.ok) {
+    const json = await resp.json();
+    return json;
   }
-  return window.wholesaleData;
+  return {};
 }
 
 async function buildWholesaleForm(table) {
   const tempForm = createEl('form');
-  await buildForm(tempForm, ['business', 'address', 'contact']);
+  await buildForm(tempForm, ['business']);
   setTimeout(async () => {
     [...tempForm.children].forEach(async (field) => {
       const row = createEl('tr', {
-        html: `<td colspan="3" class="wholesale-input">
-            ${field.getAttribute('data-category') === 'address'
-    ? field.outerHTML.replace('your', 'your business')
-    : field.outerHTML}
-          </td>`,
+        html: `<td colspan="2" class="wholesale-input">${field.outerHTML}</td>`,
       });
       table.append(row);
     });
-
-    // rearrange addr, VERY HACKY
-    const city = table.querySelector('input#city').parentNode;
-    const state = table.querySelector('input#state').parentNode;
-    const zip = table.querySelector('input#zip').parentNode;
-
-    const newRow = createEl('tr', {
-      html: `<td class="wholesale-input">${city.innerHTML}</td>
-        <td class="wholesale-input">${state.innerHTML}</td>
-        <td class="wholesale-input">${zip.innerHTML}</td>`,
-    });
-
-    city.parentNode.replaceWith(newRow);
-    city.remove();
-    state.parentNode.remove();
-    zip.parentNode.remove();
 
     const data = await fetchWholesaleData();
     Object.keys(data).forEach((key) => {
       const typeHead = createEl('tr', {
         html: `<td>${key.replace('Case', '')}</td>
-          <td>In Stock</td>
           <td>Quantity</td>`,
         class: 'wholesale-type-heading',
       });
       table.append(typeHead);
       data[key].forEach((item) => {
         const row = createEl('tr', {
-          html: `<td>${item.item} ${item.type}</td>
-            <td>${item.remaining}</td>
+          html: `<td>
+              <p>${item.item} ${item.type} ${item.remaining <= 10 ? '<span class="wholesale-low-stock">just a few left!</span>' : ''}</p>
+              ${item.description ? `<p class="wholesale-item-desc">${item.description}</p>` : ''}
+            </td>
             <td class="wholesale-input">
               <input type="number" value="0" min="0" max="${item.remaining}" step="1" id="${item.id}" name="${item.id}" data-name="${item.item}" data-type="${item.type}" class="wholesale-input-quantity" />
             </td>`,
@@ -169,35 +149,58 @@ function closeWholesaleForm() {
 async function postWholesaleOrder(data) {
   const params = await buildOrderParams(data);
   const qs = buildGQs(params);
-  // const url = buildGScriptLink(WHOLESALE_KEY);
-  console.log('qs:', qs);
-  // const orderConfirmation = await fetch(`${url}?${qs}`, {
-  //   method: 'POST',
-  //   headers: {
-  //     Accept: 'application/json',
-  //   },
-  // })
-  //   .catch((error) => {
-  //     // eslint-disable-next-line no-console
-  //     console.error(error);
-      // return makeScreensaverError('something went wrong and your order didn\'t go through. try again?');
-  //   })
-  //   .then((resp) => {
-  //     if (!resp.ok) {
-  //       return resp.text().then((errorInfo) => { Promise.reject(errorInfo); });
-  //     }
-  //     return resp.text();
-  //   }).then((text) => {
-  //     const obj = JSON.parse(text);
-  //     console.log('obj:', obj);
-  //     // if (typeof obj.order !== 'undefined') {
-  //     //   return obj.order;
-  //     // }
-  //     // eslint-disable-next-line no-console
-  //     console.error('errors:', data);
-      return makeScreensaverError('something went wrong and your order didn\'t go through. try again?');
-  //   });
-  // return orderConfirmation;
+  const url = buildGScriptLink(WHOLESALE_KEY);
+  const resp = await fetch(`${url}?${qs}`, { method: 'POST' });
+  const json = await resp.json();
+  return json;
+}
+
+async function updateWholesaleAfterSubmission(form) {
+  const fields = form.querySelectorAll('.wholesale-input input');
+  fields.forEach((field) => {
+    if (field.value <= 0) { // unordered item
+      const row = field.closest('tr');
+      row.remove();
+    } else {
+      field.disabled = true;
+    }
+  });
+  const headers = form.querySelectorAll('tr.wholesale-type-heading');
+  headers.forEach((header) => {
+    const next = header.nextElementSibling;
+    if (!next || [...headers].includes(next)) header.remove();
+  });
+  const footer = document.querySelector('.wholesale-foot');
+  const footerBtn = footer.querySelector('a');
+  if (footerBtn) footerBtn.remove();
+  // thank you message
+  const wrapper = createEl('div', {
+    class: 'payment-success-message',
+  });
+  const labels = await fetchLabels();
+  const message = labels.wholesale_thankyou.split('\n');
+  message.forEach((line) => {
+    const p = createEl('p', {
+      text: line,
+    });
+    wrapper.append(p);
+  });
+  // contact button
+  const phone = labels.store_phone;
+  const contactBtn = createEl('a', {
+    class: 'btn btn-rect btn-fixed',
+    text: phone,
+    href: `sms:+${phone.replace(/\D/g, '')}`,
+  });
+  wrapper.append(contactBtn);
+  // new order btn
+  const newOrderBtn = createEl('a', {
+    class: 'btn btn-rect btn-fixed',
+    text: 'submit a new wholesale order',
+  });
+  newOrderBtn.addEventListener('click', () => window.location.reload());
+  wrapper.append(newOrderBtn);
+  footer.append(wrapper);
 }
 
 async function submitWholesaleOrder() {
@@ -210,12 +213,16 @@ async function submitWholesaleOrder() {
     // get submission data
     const data = getSubmissionData(form);
     saveToLocalStorage(form);
-    console.log('form data:', data);
     // submit order
     const order = await postWholesaleOrder(data);
-    console.log('order:', order);
+    if (order.result === 'success') {
+      // update wholesale form
+      updateWholesaleAfterSubmission(form);
+      removeScreensaver();
+    } else {
+      makeScreensaverError('something went wrong and your order didn\'t go through.');
+    }
   }
-  // update wholesale form
 }
 
 function setupWholesaleForm(a, main) {
@@ -245,13 +252,34 @@ function setupWholesaleForm(a, main) {
     // setup place order btn
     const submitBtn = wholesaleForm.querySelector('.wholesale-foot a.btn');
     submitBtn.textContent = 'submit wholesale order';
-    submitBtn.addEventListener('click', async(e) => {
+    submitBtn.addEventListener('click', async (e) => {
       submitWholesaleOrder(e);
     });
 
     wrapper.append(wholesaleForm);
     main.append(wrapper);
     loadCSS('/assets/blocks/checkout/checkout.css');
+  }
+}
+
+async function checkIfWholesaleOpen(main) {
+  const now = new Date();
+  const open = new Date();
+  const close = new Date();
+  // eslint-disable-next-line no-mixed-operators
+  open.setDate(open.getDate() + ((7 - open.getDay()) % 7 + 6) % 7); // 6 = saturday
+  open.setHours((12 + 3), 0, 0); // 3:00 pm
+  // eslint-disable-next-line no-mixed-operators
+  close.setDate(close.getDate() + ((7 - close.getDay()) % 7 + 2) % 7); // 2 = tuesday
+  close.setHours((12 + 3), 5, 0); // 3:05 pm
+  if (now > open && now < close) { // ordering is open
+    main.querySelectorAll('a[href*="#order"]').forEach((a) => setupWholesaleForm(a, main));
+  } else {
+    const labels = await fetchLabels();
+    main.querySelectorAll('a[href*="#order"]').forEach((a) => {
+      const msg = createEl('p', { text: labels.wholesale_closed });
+      a.replaceWith(msg);
+    });
   }
 }
 
@@ -284,7 +312,7 @@ export default async function decorateOrder(main) {
     });
     // setup wholesale order form
     if (window.location.pathname.includes('/wholesale')) {
-      main.querySelectorAll('a[href*="order"]').forEach((a) => setupWholesaleForm(a, main));
+      checkIfWholesaleOpen(main);
     }
     // replace title on abnormal
     if (window.location.pathname.includes('/abnormal')) {
